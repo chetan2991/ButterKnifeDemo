df213aa1fefbeab9fbdd399b96f12e68fd579932
chetan shingare <chetanshingare1992@gmail.com>
2016-05-25 10:09:35 +0530

    initial commit

---
+++ app/src/main/java/software/ram/com/butterknifedemo/MainActivity.java
@@ -0,0 +1,13 @@
+package software.ram.com.butterknifedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
